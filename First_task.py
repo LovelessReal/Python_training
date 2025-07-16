8312768a48153412b547fd7b45a5ea8afaeb541a
Valeria Andreevna <lovelessreal@gmail.com>
2025-07-16 13:01:26 +0300

    Update First_task.py

--- First_task.py
+++ First_task.py
@@ -1,1 +1,18 @@
-print('Hello world')
+"Hello world" #при использовании данных типа string используются кавычки.
+'Hello world' #но стандарт в python - кавычки одинарные
+
+My_name = 'Valeria'  #присваиваем строковое значение переменной
+
+print('Hello world') 
+
+my_age = 28 #присваиваем числовое значение для переменной
+
+print (my_age) 
+
+var1 = True #работа с булевыми значениями не требует спец символов
+var2 = False
+
+def calculate(x, y): #обозначаем функцию
+ print (x+2/y-1) 
+
+calculate(1,3)
